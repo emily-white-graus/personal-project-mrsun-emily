bb4d67bd795bdd17b8c8e6969707b9f7cc036834
Emily White Graus <montserratcardona@KD-B99084246.local>
2026-05-25 23:31:43 +0200

    Routing mrsun

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import { Link } from "expo-router"
+import { StyleSheet, Text, View } from "react-native"
+
+import CurrentSun from "../../CurrentSun"
+import Forecast from "../../Forecast"
+
+const location = { name: "Barcelona", latitude: 41.385063, longitude: 2.173404 }
+
+const App: React.FC = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>MrSun</Text>
+
+        <CurrentSun location={location} />
+        <Forecast location={location} />
+
+        <Link href="/temp">Go to Temporary</Link>
+      </View>
+    </>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
